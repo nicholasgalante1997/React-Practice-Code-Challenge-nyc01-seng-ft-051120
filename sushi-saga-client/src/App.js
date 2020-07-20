04ffe8e95fd9b8f363d68b09634f5b934a37721c
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-07-20 11:25:52 -0400

    not one deliverable solved im absolutely crushed right now

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+import Sushi from './components/Sushi';
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: []
+  }
+
+  fetchSushi = () => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushis => 
+      this.setState({
+        sushi: sushis
+      })
+      )
+  }
+
+  componentDidMount(){
+    this.fetchSushi()
+  }
+
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,30 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
+const API = "http://localhost:3000/sushis"
 
-const SushiContainer = (props) => {
-  return (
+class SushiContainer extends React.Component {
+
+
+  render () {
+    return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
+        { 
+        // this.props.sushi.map(sushi => {
+        //  return <Sushi 
+        //   key={sushi.id}
+        //   name={sushi.name}
+        //   img_url={sushi.img_url}
+        //   price={sushi.price}
+        //   />
         }
+          
+        
         <MoreButton />
       </div>
     </Fragment>
-  )
+  )}
 }
 
 export default SushiContainer
